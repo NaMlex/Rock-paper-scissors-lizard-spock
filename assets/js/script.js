19e087567c77f407baa06b66826b6c458fcb6861
AMNlex Amnlex <alexn.alexn@gmail.com>
2025-01-10 22:02:02 +0000

    Added restart function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 //From Math Project, Code Institute and adapted for this project
 // Wait for the DOM to finish loading before running the game
+const restartBtn = document.querySelector(".restart");
+let your_score = 0;
+let computer_score = 0;
+
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.querySelectorAll(".player-choice");
@@ -10,12 +14,17 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert('You got it');
                 yourScore();
             } else {
-                alert('Try again');
+                alert(`Try again the correct answer was` );
                 computerScore();
             }
         });
     }
 });
+
+restartBtn.addEventListener('click', function(){
+    restartGame();
+});
+
 
 //Choices array 
 const choice = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
@@ -25,15 +34,27 @@ function computerChoice() {
     const pick = choice[Math.floor(Math.random() * choice.length)];
     return pick;
     }
+   
 
 //Your score 
 function yourScore(){
-    let your_score = document.querySelector(".your-score").innerText;
+    your_score = document.querySelector(".your-score").innerText;
     document.querySelector(".your-score").innerText = ++your_score;
 }
 
+
+
 //Computer Score 
 function computerScore(){
-    let computer_score = document.querySelector(".computer-score").innerText;
+    computer_score = document.querySelector(".computer-score").innerText;
     document.querySelector(".computer-score").innerText = ++computer_score;
 }
+
+//Restart
+function restartGame(){
+    your_score = 0;
+    computer_score = 0;
+    document.querySelector(".your-score").innerText = your_score;
+    document.querySelector(".computer-score").innerText = computer_score;
+}
+
